7100bd3e151b6ba630aa621a3bd79cdfe0fb4fcd
Ilya Denisov <fredperry.d1a@gmail.com>
2021-03-06 20:29:50 +0300

    Add kotlin project, and read, complete 54 list

---
+++ src/TypeIntro.kt
@@ -0,0 +1,24 @@
+const val MAX_EXPERIENCE: Int = 5000
+
+fun main(args: Array<String>) {
+    val playerName = "Aragorn"
+    val playerNameReversed = playerName.reversed()
+    var experiencePoints = 5
+    experiencePoints += 5
+    var hasSteed = false
+    val NameTavern = "Rog Edinoroga"
+    val NameTraktirshik = "Traktirshik"
+    var playerCash = 50
+    var MenuTavern = listOf("Honey", "Wine", "Beer")
+
+
+
+    println(experiencePoints)
+    println(playerName)
+    println(playerNameReversed)
+    println(hasSteed)
+    println(NameTavern)
+    println(NameTraktirshik)
+    println(playerCash)
+    println(MenuTavern)
+}
